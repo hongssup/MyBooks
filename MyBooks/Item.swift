481f24dabb61b2c1f49deeec2420c439e12b6bcb
홍서연 <hongssup0202@gmail.com>
2024-02-18 17:22:58 +0900

    Initial Commit

---
+++ MyBooks/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyBooks
+//
+//  Created by hongssup on 2/18/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
